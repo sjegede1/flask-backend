76fcd563192d2a003d951d991900c48ac3e180f3
Sola Jegede <sjegede1@gmail.com>
2023-08-10 08:17:39 -0500

    deployed built version of react app

--- app.py
+++ app.py
@@ -5,15 +5,28 @@ from flask import Flask
  
 # Flask constructor takes the name of
 # current module (__name__) as argument.
-app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
+app = Flask(__name__, static_folder='./build', static_url_path='/')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+
 @app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+
+@app.route('/hello')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return f'<h1>Sola Jegede</h1>'
+    return [{'name': 'Sola Jegede','location':'Texas'},{'name':'Ope Jegede','location':'British Columbia'}]
+
+@app.route('/home')
+def home():
+    return {'home': 'Where the heart is.'}
  
 # main driver function
 if __name__ == '__main__':
@@ -21,3 +34,7 @@ if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
     app.run()
+
+
+
+    app = Flask(__name__, static_folder='../build', static_url_path='/')
